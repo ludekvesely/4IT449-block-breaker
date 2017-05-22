9b103d52611e6060948c03f38c8c146fa17c814b
Luděk Veselý <ludek.vesely@email.com>
2017-05-22 23:40:38 +0200

    :sparkles: create highscore model

---
+++ vesl00_4IT449_semestralka/DataObjects/HighscoreDO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Data.SqlClient;
+using vesl00_4IT449_semestralka;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using vesl00_4IT449_semestralka.Models;
+
+namespace vesl00_4IT449_semestralka.DataObjects
+{
+    class HighscoreDO
+    {
+        public int Score { get; set; }
+        public string Nick { get; set; }
+
+        public static List<HighscoreDO> GetHighscores()
+        {
+            using (HighscoreEntities context =
+                new HighscoreEntities())
+            {
+                return context.highscore
+                    .Select(x => new HighscoreDO()
+                    {
+                        Nick = x.nick,
+                        Score = x.score
+                    })
+                    .OrderByDescending(x => x.Score)
+                    .ToList();
+            }
+        }
+    }
+}
